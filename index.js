d4af34586c0942706307ffc82857778c3b7a6026
jiangxue <994898372@qq.com>
2018-12-17 18:02:41 +0800

    添加鼠标事件

--- index.js
+++ index.js
@@ -5,28 +5,34 @@
 $("#img1").addClass("current");
 $("#img2").addClass("enter");
 $("#img3").addClass("enter");
-let n = 1;
-setInterval(() => {
-    $(`.wrapper > img:nth-child(${x(n)})`).removeClass('current').addClass("leave").one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass("leave").addClass('enter')
-    })
-    $(`.wrapper > img:nth-child(${x(n+1)})`).removeClass('enter').addClass("current")
-    n++;
-}, 1000);
+var n = 1;
+var timerId = start();
+function start() {
+    return setInterval(() => {
+        $(`.wrapper > img:nth-child(${x(n)})`).removeClass('current').addClass("leave").one('transitionend', (e) => {
+            $(e.currentTarget).removeClass("leave").addClass('enter')
+        })
+        $(`.wrapper > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass("current")
+        n++;
+    }, 2000);
+}
+
+$('.wrapper').on('mouseenter', function () {
+
+    window.clearInterval(timerId)
+})
+
+$('.wrapper').on('mouseleave', function () {
+    timerId = start()
+})
 
 
-
-
-
-
-
-
-function x(n){
-    if(n>3){
-      n = n%3
-      if (n===0){
-        n =3
-      }
+function x(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
+            n = 3
+        }
     } // n = 1 2 3
     return n
-  }
+}
